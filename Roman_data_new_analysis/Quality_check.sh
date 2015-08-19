18bed8b22c3e2326374faf4eab85f55aefe5aa50
Jatin Talwar <jatintalwar@users.noreply.github.com>
2015-08-19 10:24:18 +0200

    updated quality_check.sh
    added new changes

--- Roman_data_new_analysis/Quality_check.sh
+++ Roman_data_new_analysis/Quality_check.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ## script for quality check for each file in the directory
 ## note that files should be in defined format if they are 'paired end' 
 ## the files should be unzipped and be in 'fastq' format
@@ -7,8 +8,8 @@ for file1 in ~/pool-talwar/RNA-Seq_pipeline/Raw_data_files/pair1/*.fastq
 do
 PAIR1=$file1
 echo " "
-echo "`tput bold`Running fastqc for: $PAIR1`tput sgr0`"
-fastqc -o ~/pool-talwar/RNA-Seq_pipeline/Quality_control/fastqc/ -t 13 -q $PAIR1
+echo "    `tput bold`Running fastqc for: $PAIR1`tput sgr0`"
+echo "    fastqc -o ~/pool-talwar/RNA-Seq_pipeline/Quality_control/fastqc/ -t 13 -q $PAIR1"
 done
 #
 #
@@ -18,12 +19,13 @@ done
 for file2 in ~/pool-talwar/RNA-Seq_pipeline/Raw_data_files/pair2/*.fastq
 do
 PAIR2=$file2
-echo "`tput bold`Running fastqc for: $PAIR2`tput sgr0`"
-fastqc -o ~/pool-talwar/RNA-Seq_pipeline/Quality_control/fastqc/ -t 13 -q $PAIR2
+echo " "
+echo "    `tput bold`Running fastqc for: $PAIR2`tput sgr0`"
+echo "     fastqc -o ~/pool-talwar/RNA-Seq_pipeline/Quality_control/fastqc/ -t 13 -q $PAIR2"
 done
 #
 echo " "
-echo "`tput bold`the FastQC outputs are at : ~/pool-talwar/RNA-Seq_pipeline/Quality_control/fastqc/ `tputsgr0`"
+echo "`tput bold`the FastQC outputs are at : ~/pool-talwar/RNA-Seq_pipeline/Quality_control/fastqc/ `tput sgr0`"
 #
 #
 #
